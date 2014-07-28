dd3819f2d37b9cfb822705ccda54faefdee0c3ca
Sam Popowich <spopowich@gmail.com>
2014-07-28 11:46:42 -0600

    Made blacklight lib directories explicit.

---
+++ lib/blacklight/solr.rb
@@ -0,0 +1,9 @@
+# -*- encoding : utf-8 -*-
+module Blacklight::Solr
+  
+  autoload :Facets, 'blacklight/solr/facets'
+  autoload :FacetPaginator, 'blacklight/solr/facet_paginator'
+  autoload :Document, 'blacklight/solr/document'
+  autoload :Request, 'blacklight/solr/request'
+  
+end
