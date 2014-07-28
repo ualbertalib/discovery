b0bb9d7fe0d6a86e4f8f0f392e20fb3cce22f291
Sam Popowich <spopowich@gmail.com>
2014-07-28 11:52:56 -0600

    Remove blacklight lib dir as it didn't fix the problem.

--- lib/blacklight/solr.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-module Blacklight::Solr
-  
-  autoload :Facets, 'blacklight/solr/facets'
-  autoload :FacetPaginator, 'blacklight/solr/facet_paginator'
-  autoload :Document, 'blacklight/solr/document'
-  autoload :Request, 'blacklight/solr/request'
-  
-end
