1bad7db80910f105cbcc5e9901aa7748de5ffa27
Sam Popowich <spopowich@gmail.com>
2014-11-18 09:16:36 -0700

    Added ejournal and symphony controllers to bento.

--- app/controllers/bento_controller.rb
+++ app/controllers/bento_controller.rb
@@ -3,10 +3,18 @@ require "json"
 class BentoController < ApplicationController
 
   def index
-    @databases = []
-    #@databases = DatabasesController.new.find.documents.first.as_json
-    DatabasesController.new.find.documents.each do |db|
-      @databases << db.as_json["title_display"]
+    @databases = populate_from DatabasesController
+    @ejournals = populate_from EjournalsController
+    @symphony = populate_from SymphonyController
+  end
+
+  private
+
+  def populate_from(controller)
+    documents = []
+    controller.new.find.documents.each do |db|
+      documents << db.as_json["title_display"]
     end
+    documents
   end
 end
