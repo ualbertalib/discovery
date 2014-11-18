872ca61ba9a3a79992b6f3a2480c021437fd2427
Sam Popowich <spopowich@gmail.com>
2014-11-18 13:51:23 -0700

    Add hit counts to bento logic.

--- app/controllers/bento_controller.rb
+++ app/controllers/bento_controller.rb
@@ -3,18 +3,30 @@ require "json"
 class BentoController < ApplicationController
 
   def index
-    @databases = populate({format: 'Database'})
-    @ejournals = populate({source: 'SFX'})
-    @symphony = populate({source: 'Symphony'})
+    databases = populate({format: 'Database'})
+    @database_count = databases["count"]
+    databases.delete("count")
+    @databases = databases
+
+    ejournals = populate({source: 'SFX'})
+    @ejournals_count = ejournals["count"]
+    ejournals.delete("count")
+    @ejournals = ejournals
+
+    symphony = populate({source: 'Symphony'})
+    @symphony_count = symphony["count"]
+    symphony.delete("count")
+    @symphony = symphony
   end
 
   private
 
   def populate(criterion)
-    documents = []
+    documents = {}
     (@solr_response, @document_list) = CatalogController.new.get_search_results(:q => '', :f => criterion)
+    documents["count"] = @solr_response["response"]["numFound"]
     @document_list.each do |db|
-      documents << db.as_json["title_display"]
+      documents[db.as_json["id"]] = db.as_json["title_display"]
     end
     documents
   end
