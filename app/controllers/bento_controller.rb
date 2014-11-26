e16b0dbb19e079e18f3fe394a005eeed7acf4cf1
Sam Popowich <spopowich@gmail.com>
2014-11-26 11:02:30 -0700

    Refactor bento controller: first pass.

--- app/controllers/bento_controller.rb
+++ app/controllers/bento_controller.rb
@@ -5,39 +5,50 @@ class BentoController < ApplicationController
   include Blacklight::Catalog
 
   def index
-    databases = populate({format: 'Database'})
+    @complete_count = 0
+
+    databases = populate("Catalog", {format: 'Database'})
     @database_count = databases["count"]
     databases.delete("count")
     @databases = databases
 
-    ejournals = populate({source: 'SFX'})
+    ejournals = populate("Catalog", {source: 'SFX'})
     @ejournals_count = ejournals["count"]
     ejournals.delete("count")
     @ejournals = ejournals
 
-    symphony = populate({source: 'Symphony'})
+    symphony = populate("Catalog", {source: 'Symphony'})
     @symphony_count = symphony["count"]
     symphony.delete("count")
     @symphony = symphony
 
-    eds = get_eds_results
+    eds = populate("Articles", {})
     @eds_count = eds["count"]
     eds.delete("count")
     @eds = eds
     
-    @complete_count = @symphony_count + @ejournals_count + @database_count + @eds_count
   end
 
   private
 
-  def populate(criterion)
+  def populate(controller_name, criterion)
+    controller = (controller_name+"Controller").constantize
     @query = params[:q]
     documents = {}
-    (@solr_response, @document_list) = CatalogController.new.get_search_results(:q => @query, :f => criterion)
+    (@solr_response, @document_list) = controller.new.get_search_results(:q => @query, :f => criterion)
     documents["count"] = @solr_response["response"]["numFound"]
+    @complete_count += documents["count"]
     @document_list.each do |doc|
+      metadata = populate_metadata(doc)
+      documents[doc.as_json["id"]] = metadata
+    end
+    documents
+  end
+
+  def populate_metadata(doc)
       metadata = {}
       metadata[:title] = doc.as_json["title_display"]
+      metadata[:subtitle] = doc.as_json["subtitle_display"]
       metadata[:author] = doc.as_json["author_display"]
       metadata[:isbn] = doc.as_json["isbn_t"]
       metadata[:issn] = doc.as_json["issn_t"]
@@ -48,24 +59,6 @@ class BentoController < ApplicationController
       #Ejournals: coverage statement
       #Articles: full text?, Link to PDF, Year of publication - not sure
       #these are possible. Depends on EDS API
-      documents[doc.as_json["id"]] = metadata
-    end
-    documents
-  end
-
-  def get_eds_results
-    @query = params[:q]
-    documents = {}
-    (@solr_response, @document_list) = ArticlesController.new.get_search_results(:q => @query) #, :f => {format: 'Serial'})
-    documents["count"] = @solr_response["response"]["numFound"]
-    @document_list.each do |doc|
-      metadata = {}
-      metadata[:title] = doc.as_json["title_display"]
-      metadata[:subtitle] = doc.as_json["subtitle_display"]
-      metadata[:author] = doc.as_json["author_display"].split.first
-      metadata[:year] = doc.as_json["pub_date"]
-      documents[doc.as_json["id"]] = metadata
-     end
-    documents
+      metadata
   end
 end
