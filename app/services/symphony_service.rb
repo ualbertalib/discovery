d9bf01534cfae9f9fcfa5e8f57ef270fa622fc7f
Sam Popowich <spopowich@gmail.com>
2015-03-12 12:56:08 -0600

    Change WS endpoint.

--- app/services/symphony_service.rb
+++ app/services/symphony_service.rb
@@ -3,12 +3,12 @@ class SymphonyService
   def initialize
     @ws_endpoint = "https://ws.library.ualberta.ca/symws3/rest/standard/"
     @method = "lookupTitleInfo"
-    @parameters = "?clientID=Primo&marcEntryFilter=ALL&includeAvailabilityInfo=true&titleID="
+    @parameters = "?clientID=Primo&marcEntryFilter=ALL&includeItemInfo=true&titleID="
   end
 
   def get_status(id, library)
     url = @ws_endpoint+@method+@parameters+id+"&libraryFilter="+library
     document = Nokogiri::XML(open(url).read)
-    document.xpath("//xmlns:locationOfFirstAvailableItem", :xmlns=>"http://schemas.sirsidynix.com/symws/standard").text
+    document.xpath("//xmlns:currentLocationID", :xmlns=>"http://schemas.sirsidynix.com/symws/standard").text
   end
 end
