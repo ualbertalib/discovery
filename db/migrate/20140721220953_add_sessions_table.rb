1d87ed9672caec4a25f7aa2d4c2de0fe9ab37fb7
Sam Popowich <spopowich@gmail.com>
2015-04-21 13:56:28 -0600

    Change session DB column size.

--- db/migrate/20140721220953_add_sessions_table.rb
+++ db/migrate/20140721220953_add_sessions_table.rb
@@ -2,7 +2,7 @@ class AddSessionsTable < ActiveRecord::Migration
   def change
     create_table :sessions do |t|
       t.string :session_id, :null => false
-      t.text :data
+      t.column :data, :longtext
       t.timestamps
     end
 
