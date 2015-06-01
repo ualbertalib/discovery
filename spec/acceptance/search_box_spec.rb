1160ba0264f8df061687ad2c907b40119cb21560
Sam Popowich <spopowich@gmail.com>
2015-06-01 14:17:41 -0600

    Get search_box test to work remotely.

--- spec/acceptance/search_box_spec.rb
+++ spec/acceptance/search_box_spec.rb
@@ -4,9 +4,13 @@ describe 'home page' do
   include Capybara::DSL
 
   it "provides a search box" do
+    Capybara.run_server = false
+    Capybara.current_driver = :selenium
+    Capybara.app_host = "https://search-test.library.ualberta.ca"
     visit '/'
     fill_in 'q', with: "Global Warming"
     click_button 'search'
     expect(current_path).to eq "/results"
    end
+
 end
