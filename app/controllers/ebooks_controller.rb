d8ee5596f64dafd240eb2b02b958594fef9673cc
Sam Popowich <spopowich@gmail.com>
2015-06-04 13:00:44 -0600

    Fix Ebooks controller. Closes #254.

--- app/controllers/ebooks_controller.rb
+++ app/controllers/ebooks_controller.rb
@@ -8,7 +8,7 @@ class EbooksController < CatalogController
 
   def show_only solr_parameters, user_parameters
     solr_parameters[:fq] ||= []
-    solr_parameters[:fq] << 'owning_library_tesim:"UAINTERNET"'
+    solr_parameters[:fq] << 'location_tesim:"uainternet"'
     solr_parameters[:fq] << 'format:"Book"'
   end
 
