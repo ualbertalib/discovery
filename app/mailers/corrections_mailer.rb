527e3b9ceb4330a15aa94443a880cfa9c896db40
Sam Popowich <spopowich@gmail.com>
2015-07-20 15:42:08 -0600

    Replace test helpdesk email with correct address.

--- app/mailers/corrections_mailer.rb
+++ app/mailers/corrections_mailer.rb
@@ -6,7 +6,6 @@ class CorrectionsMailer < ApplicationMailer
   def corrections_email(item_id, message)
     @item_id = item_id
     @message = message
-    #mail(to: "libraryhelpdesk@ualberta.ca", subject: "Cataloguing Correction Submitted")
-    mail(to: "spopowic@ualberta.ca", subject: "Cataloguing Correction Submitted")
+    mail(to: "libraryhelpdesk@ualberta.ca", subject: "Cataloguing Correction Submitted")
   end
 end
