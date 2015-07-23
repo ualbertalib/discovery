1e4bbe9f82d5d9907996f1d0a00d917d7aefea9c
Sam Popowich <spopowich@gmail.com>
2015-07-23 11:16:39 -0600

    Fix bug in location facet display.

--- app/helpers/blacklight/facets_helper_behavior.rb
+++ app/helpers/blacklight/facets_helper_behavior.rb
@@ -195,7 +195,11 @@ module Blacklight::FacetsHelperBehavior
     facet_config = facet_configuration_for_field(field)
     
     value = if item.respond_to? :label
-      item.label
+        if @locations[item.label]
+          @locations[item.label]
+        else
+          item.label
+        end
     else
       facet_value_for_facet_item(item)
     end
