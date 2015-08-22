811d364fff704c659c6ab0aef34558c0abe46890
Sam Popowich <spopowich@gmail.com>
2015-08-22 08:33:13 -0600

    Add search history feature spec.

---
+++ spec/features/search_history_spec.rb
@@ -0,0 +1,25 @@
+require_relative "../rake_helper"
+require_relative "../spec_helper"
+
+include RakeTasks
+
+initialize_rake_tasks
+delete_solr_index
+ingest_all_test_sets
+
+RSpec.feature "Search history test", :type => :feature do
+  scenario "User wants to see their search history" do
+    visit "/search_history"
+    if page.to_s.include? "Your recent searches"
+      click_button "Clear Search History"
+    end
+    expect(page).to have_text "You have no search history"
+    visit "/catalog"
+    fill_in "q", :with => "accountancy"
+    click_button "search"
+    visit "/search_history"
+    expect(page).to have_text("Search History")
+    expect(page).to have_text("Keyword:accountancy")
+  end
+end
+
