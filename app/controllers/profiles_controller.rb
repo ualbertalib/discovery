152a0b8da21316ad5f178a6e13587a3bb160e721
nnunn <natasha@nnunn.ca>
2015-10-16 15:23:52 -0600

    finishing profile app

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,5 @@
 class ProfilesController < ApplicationController
+	http_basic_authenticate_with name: "ualibraries", password: "discoverthi5", except: [:index, :show]
 	
 	def index
     	@profiles = Profile.all.order(:first_name)
@@ -9,6 +10,11 @@ class ProfilesController < ApplicationController
 	end
 
 	def new
+		@profile = Profile.new
+	end
+
+	def edit
+		@profile = Profile.find(params[:id])
 	end
 
 	def create
@@ -18,8 +24,25 @@ class ProfilesController < ApplicationController
   		redirect_to @profile
 	end
 
-	private
+	def update
+  		@profile = Profile.find(params[:id])
+ 
+		if @profile.update(profile_params)
+			redirect_to @profile
+		else
+    		render 'edit'
+  		end
+	end
 
+	def destroy
+  		@profile = Profile.find(params[:id])
+  		@profile.destroy
+ 
+  		redirect_to profiles_path
+	end
+
+	private
+	
 	def profile_params
 		params.require(:profile).permit(:first_name,:last_name, :job_title, :unit, :email, :phone, :campus_address, :expertise, :introduction, :publications, :staff_since, :links, :orcid, :committees, :personal_interests, :opt_in)
 	end
