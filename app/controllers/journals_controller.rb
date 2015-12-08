10936cfaf12370b9f82164f2512b83330684893d
Sam Popowich <spopowich@gmail.com>
2015-12-08 10:33:59 -0700

    Change 'Serial' format back to 'Journal'. Issue #793.

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -8,7 +8,7 @@ class JournalsController < CatalogController
   
   def show_only solr_parameters, user_parameters
     solr_parameters[:fq] ||= []
-    solr_parameters[:fq] << 'format: "Serial"'
+    solr_parameters[:fq] << 'format: "Journal"'
   end
 
   def index
