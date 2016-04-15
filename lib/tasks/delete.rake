e4454491667db867d032f97733e9732cad9c9b5b
Sam Popowich <spopowich@gmail.com>
2016-04-15 11:51:25 -0600

    Fix bento controller spec.

--- lib/tasks/delete.rake
+++ lib/tasks/delete.rake
@@ -4,8 +4,8 @@ require 'rsolr'
 
 desc 'Delete all records from solr index'
 task :delete, [:records] do |t, args|
-  @c = IngestConfiguration.new(args.records || "databases", @config_file)
-  solr = RSolr.connect :url=>@c.solr
+  c = IngestConfiguration.new(args.records || "databases", @config_file)
+  solr = RSolr.connect :url=>c.solr
   case args.records
 
   when "databases"
--- spec/controllers/bento_controller_spec.rb
+++ spec/controllers/bento_controller_spec.rb
@@ -24,7 +24,7 @@ describe BentoController, type: :controller do
     expect(database_count).to eq 10
     expect(databases.size).to eq database_count
     expect(databases.first).to be_a Array
-    expect(databases["11710064"].to_s).to eq '{:title=>"Advances in Polymer Science", :isbn=>nil, :issn=>nil, :year=>nil, :call_number=>nil, :format=>["Database"], :electronic=>["Online"]}'
+    expect(databases["11710064"].to_s).to eq '{:title=>"Advances in Polymer Science", :isbn=>nil, :issn=>nil, :year=>nil, :call_number=>nil, :format=>["Database"], :electronic=>["Online"], :source=>nil, :locations=>nil}'
     expect(databases["11710100"][:title]).to eq "America: History and Life with Full Text"
     expect(databases["11710328"][:title]).to eq "Canadiana"
   end
@@ -32,12 +32,11 @@ describe BentoController, type: :controller do
   it "should populate journal results" do
     journals_count = assigns(:journals_count)
     journals = assigns(:journals)
-    expect(journals_count).to eq 65
+    expect(journals_count).to eq 14 
     expect(journals.size).to eq journals_count
     expect(journals.first).to be_a Array
-    expect(journals["1000163"].to_s).to eq '{:title=>"Infection control and hospital epidemiology", :isbn=>nil, :issn=>nil, :year=>["1988"], :call_number=>nil, :format=>["Serial"], :electronic=>["Online"]}'
-    expect(journals["1000174"][:title]).to eq "Libraries & culture"
-    expect(journals["954921332001"][:title]).to eq "Publishers weekly"
+    expect(journals["954921332001"].to_s).to eq '{:title=>"Publishers weekly", :isbn=>nil, :issn=>nil, :year=>nil, :call_number=>nil, :format=>["Journal"], :electronic=>["Online"], :source=>["SFX"], :locations=>["uainternet"]}'
+    expect(journals["954921333006"][:title]).to eq "Absatzwirtschaft"
   end
 
   it "should populate symphony results" do
@@ -46,7 +45,7 @@ describe BentoController, type: :controller do
     expect(symphony_count).to eq 10000
     expect(symphony.size).to eq 100 # because a limit is set in the controller
     expect(symphony.first).to be_a Array
-    expect(symphony["100000"].to_s).to eq  '{:title=>"All about winter safety", :isbn=>nil, :issn=>nil, :year=>["1975"], :call_number=>nil, :format=>["Book"], :electronic=>["At Library"]}'
+    expect(symphony["100000"].to_s).to eq  '{:title=>"All about winter safety", :isbn=>nil, :issn=>nil, :year=>["1975"], :call_number=>nil, :format=>["Book"], :electronic=>["At Library"], :source=>["Symphony"], :locations=>["uabard"], :ual=>true}'
     expect(symphony["1000000"][:title]).to eq "Wage and salary administration"
     expect(symphony["1000000"][:electronic].first).to eq "At Library"
     expect(symphony["1000010"][:title]).to eq "Enquête sur le salaire annuel garanti"
