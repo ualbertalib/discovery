61414ba5e5bc36914bbddd5beb24a77a385eab8e
Sam Popowich <spopowich@gmail.com>
2016-07-19 10:16:27 -0600

    Reenable institution filter.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -119,7 +119,7 @@ class CatalogController < ApplicationController
     # :show may be set to false if you don't want the facet to be drawn in the
     # facet bar
     config.add_facet_field 'electronic_tesim', :label => 'Access', collapse: false
-    #config.add_facet_field 'institution_tesim', :label => 'Institution'
+    config.add_facet_field 'institution_tesim', :label => 'Institution'
     config.add_facet_field 'location_tesim', :label => 'Library', sort: 'index'
     config.add_facet_field 'lc_1letter_facet', :label => 'Call Number', :limit => 10
     config.add_facet_field 'format', :label => 'Format', :limit => 10
