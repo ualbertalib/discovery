80983d77cf4ec52c014e326621527e28fa882bd1
Neil MacGregor <neil.macgregor@ualberta.ca>
2017-10-24 09:50:15 -0600

    Adjusted duration of config.remember_for from 1 day, to 6 hours, to deal with severe disk space use in MySQL

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -126,7 +126,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
-  config.remember_for = 1.days
+  config.remember_for = 6.hours
 
   # If true, extends the user's remember period when remembered via cookie.
   # config.extend_remember_period = false
