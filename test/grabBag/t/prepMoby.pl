656b916b17abf03faf891bccb002a8f26caea6bf
Neil MacGregor <neil.macgregor@ualberta.ca>
2018-08-20 09:03:18 -0600

    Gutenberg.org started gzipping their downloads: this updated test now zcats the gzipped file

--- test/grabBag/t/prepMoby.pl
+++ test/grabBag/t/prepMoby.pl
@@ -7,7 +7,9 @@ use strict;
 
 my $bigWhale = "/var/tmp/mobyDick.txt";
 #  Retrieve the text of Moby Dick from gutenberg.org, but only once, eh?
-`wget -O $bigWhale http://www.gutenberg.org/ebooks/2701.txt.utf-8` unless -e $bigWhale;
+`wget -O $bigWhale.gz http://www.gutenberg.org/ebooks/2701.txt.utf-8` unless -e $bigWhale;
+
+# Huh, Gutenberg started silently gzipping their downloads at one point: that's smart.  Also, annoying, since I have to deal with it
 
 
 # To be clear, this file doesn't actually contain any commas, even though it's .csv -- the format is one-search-term-per-line
@@ -16,4 +18,4 @@ my $bigWhale = "/var/tmp/mobyDick.txt";
 #
 #`sort -R /var/tmp/mobyDick.txt  | sed 's/,//g' | awk -F" " '{ print \$1 }' > /var/tmp/mobyRandom.csv`;  # Randomize it every time -- combatting Solr's caching efforts
 #`sort -R /var/tmp/mobyDick.txt  | sed 's/,//g' | awk -F" " '{ print \$1, \$2 ;}' > /var/tmp/mobyRandom.csv`;  # Randomize it every time -- combatting Solr's caching efforts
-`sort -R /var/tmp/mobyDick.txt  | sed 's/,//g' | awk -F" " '{ print \$1, \$2, \$3;}' > /var/tmp/mobyRandom.csv`;  # Randomize it every time -- combatting Solr's caching efforts
+`zcat $bigWhale.gz | sort -R | sed 's/,//g' | awk -F" " '{ print \$1, \$2, \$3;}' | sed 's/-//g' | egrep -v 'AND\$' | grep -v '^AND' | grep -v '^"AND' > /var/tmp/mobyRandom.csv`;  # Randomize it every time -- combatting Solr's caching efforts
