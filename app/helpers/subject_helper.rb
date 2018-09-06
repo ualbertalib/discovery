f4d1d80d82bdc1c6a7fee103474d949497f915c0
Natasha Nunn <natasha@nnunn.ca>
2018-09-06 07:44:37 -0600

    fixing rubocop issues

--- app/helpers/subject_helper.rb
+++ app/helpers/subject_helper.rb
@@ -1,16 +1,13 @@
 module SubjectHelper
-
-   	def subject_query(subject, index)
-   		i = 0
-        sbstring = "" 
-        while( i <= index )
-       		sbstring = sbstring + subject[i].gsub(".","").gsub("&","").gsub("+"," ")
-            	unless i == index
-                	sbstring = sbstring
-                end
-            i+= 1
-        end
-   		subject_query = "\"#{sbstring}\""
-   	end
-   	
+     def subject_query(subject, index)
+       i = 0
+       sbstring = "" 
+       while i <= index
+          sbstring += subject[i].delete(".").delete("&").tr("+", " ")
+          sbstring = sbstring unless i == index
+          i+= 1
+       end
+       sbstring = "\"#{sbstring}\""
+       sbstring
+     end  
 end
