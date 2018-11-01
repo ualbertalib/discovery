5b429ec45fe20594a65d74ee30afab9bde435292
Murny <murnagha@gmail.com>
2018-11-01 12:05:50 -0600

    Show catalog item url instead of ID in email body for corrections

--- config/environments/uat.rb
+++ config/environments/uat.rb
@@ -1,3 +1,5 @@
+Rails.application.routes.default_url_options = { host: 'uat.library.ualberta.ca', port: 3002 }
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
