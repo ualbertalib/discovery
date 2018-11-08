f3a9c1c5a671c3c4e9567e50aa7a1a73b6b654b0
Shane Murnaghan <murnagha@gmail.com>
2018-11-08 16:20:59 -0700

    WIP: Working emails, just i18n stuff left

--- app/controllers/bpsc_read_on_site_requests_controller.rb
+++ app/controllers/bpsc_read_on_site_requests_controller.rb
@@ -8,10 +8,10 @@ class BPSCReadOnSiteRequestsController < ApplicationController
 
     if @bpsc_read_on_site_request.valid?
       RequestFormMailer.bpsc_read_on_site_request(@bpsc_read_on_site_request).deliver_now
-      flash[:success] = 'Thank you! We have received your correction and will be reviewing it soon.'
+      flash[:success] = t('request_form_success')
       redirect_to root_path
     else
-      flash[:error] = 'There was an error sending your message. Please try again.'
+      flash[:error] = t('request_form_error')
       render :new
     end
   end
--- app/controllers/rcrf_read_on_site_requests_controller.rb
+++ app/controllers/rcrf_read_on_site_requests_controller.rb
@@ -8,10 +8,10 @@ class RCRFReadOnSiteRequestsController < ApplicationController
 
     if @rcrf_read_on_site_request.valid?
       RequestFormMailer.rcrf_read_on_site_request(@rcrf_read_on_site_request).deliver_now
-      flash[:success] = 'Thank you! We have received your correction and will be reviewing it soon.'
+      flash[:success] = t('request_form_success')
       redirect_to root_path
     else
-      flash[:error] = 'There was an error sending your message. Please try again.'
+      flash[:error] = t('request_form_error')
       render :new
     end
   end
