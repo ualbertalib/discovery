3f59f59c55664bb15d84d8631a95d93d6988433d
Murny <murnagha@gmail.com>
2018-11-09 13:24:28 -0700

    I18n Refactor

--- app/mailers/request_form_mailer.rb
+++ app/mailers/request_form_mailer.rb
@@ -2,18 +2,18 @@ class RequestFormMailer < ApplicationMailer
   def bpsc_read_on_site_request(bpsc_read_on_site_request)
     @bpsc_read_on_site_request = bpsc_read_on_site_request
     mail(to: ['jeff.papineau@ualberta.ca', 'kzak@ualberta.ca'],
-         subject: 'BPSC Retrieval Request (Read on Site)')
+         subject: t('bpsc_read_on_site_requests.new.header'))
   end
 
   def rcrf_read_on_site_request(rcrf_read_on_site_request)
     @rcrf_read_on_site_request = rcrf_read_on_site_request
     mail(to: ['depository@ualberta.ca', 'ill@ualberta.ca'],
-         subject: 'Request to View RCRF No Loan Items')
+         subject: t('rcrf_read_on_site_requests.new.header'))
   end
 
   def rcrf_special_request(rcrf_special_request)
     @rcrf_special_request = rcrf_special_request
     mail(to: ['depository@ualberta.ca', 'ill@ualberta.ca'],
-         subject: 'RCRF Depository Library Request Form (Microform/Newspapers)')
+         subject: t('rcrf_special_requests.new.header'))
   end
 end
