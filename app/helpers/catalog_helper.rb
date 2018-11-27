0c9c79101e435ebb0aa8bfdc48b2277c85988f87
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2018-11-27 12:49:26 -0700

    refactor 'University of Alberta Access' logic to helper

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -1,6 +1,19 @@
 module CatalogHelper
   include Blacklight::CatalogHelperBehavior
 
+  def ual_electronic_access_label(location)
+    loc = location.split(":").last.strip.gsub("University of Alberta Access", "").strip
+    if loc.start_with?('(') && loc.end_with?(')') && loc.count('(') == 1 && loc.count(')') == 1
+        loc=loc.delete('()').strip
+    end
+    return 'Click Here for University of Alberta Access' if loc.blank?
+    return "Click Here for University of Alberta Access (#{loc})"
+  end
+
+  def ual_electronic_access_url(location,url)
+    electronic_access_url(location.include?("Free Access"), url)
+  end
+
   def database_electronic_access_url(document)
     electronic_access_url(document["enableproxy_tesim"].first == "1", document["url_tesim"].first)
   end
