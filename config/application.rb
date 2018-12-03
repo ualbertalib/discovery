6e8ccbf890bc2caade35a9049625bda8835b29f7
Murny <murnagha@gmail.com>
2018-12-03 12:38:14 -0700

    Change application name to Discovery

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'csv'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module VanillaBlacklight
+module Discovery
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
