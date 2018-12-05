cebe22ba73fcc8bd456f55fcb580ad043300e92b
piyapongch <charoenw@ualberta.ca>
2018-12-05 15:40:40 -0700

    Removed blank lines

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -275,8 +275,6 @@ class CatalogController < ApplicationController
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
     config.spell_max = 5
-    config.add_show_field 'id', :label => "Kule Collection ID"
-
+    config.add_show_field 'id', label: 'Kule Collection ID'
   end
-
 end
