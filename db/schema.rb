af0773ac9bf0e9d47f850462e9ec9d15a458ccc8
pojoyoho <pojoyoho@gmail.com>
2019-03-19 13:56:20 -0600

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029171613) do
+ActiveRecord::Schema.define(version: 20190207211157) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,   null: false
