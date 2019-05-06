f5983ec600e945eb7c464002bb76afa5d0e4361d
ConnorSheremeta <sheremet@ualberta.ca>
2019-05-06 10:15:27 -0600

    Add feature tests for search results per page
    - Added feature tests for setting search results per page to 25, 50, and 100

---
+++ spec/features/search_results_per_page_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'Search sorting', type: :feature do
+  scenario 'User can change the results per page' do
+    visit '/catalog'
+    fill_in 'q', with: ' '
+    click_button 'search'
+
+    expect(page).to have_text('25 per page')
+    expect(page).to have_text('1 - 25')
+
+    find('#per_page-dropdown').click
+    click_link '50'
+
+    expect(page).to have_text('50 per page')
+    expect(page).to have_text('1 - 50')
+
+    find('#per_page-dropdown').click
+    click_link '100'
+
+    expect(page).to have_text('100 per page')
+    expect(page).to have_text('1 - 100')
+  end
+end
