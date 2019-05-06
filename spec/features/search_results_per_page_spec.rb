2c5837f25eac2622c20a06e6443bd15de5245232
ConnorSheremeta <sheremet@ualberta.ca>
2019-05-06 14:11:59 -0600

    Actually count the number of documents

--- spec/features/search_results_per_page_spec.rb
+++ spec/features/search_results_per_page_spec.rb
@@ -6,18 +6,21 @@ RSpec.describe 'Search results per page', type: :feature do
     fill_in 'q', with: ' '
     click_button 'search'
 
+    page.assert_selector('#documents .document', count: 25)
     expect(page).to have_text('25 per page')
     expect(page).to have_text('1 - 25')
 
     find('#per_page-dropdown').click
     click_link '50'
 
+    page.assert_selector('#documents .document', count: 50)
     expect(page).to have_text('50 per page')
     expect(page).to have_text('1 - 50')
 
     find('#per_page-dropdown').click
     click_link '100'
 
+    page.assert_selector('#documents .document', count: 100)
     expect(page).to have_text('100 per page')
     expect(page).to have_text('1 - 100')
   end
