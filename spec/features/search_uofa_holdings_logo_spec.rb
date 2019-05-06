1ebd3a1b7bc7b668daa30be3964967b546eed076
ConnorSheremeta <sheremet@ualberta.ca>
2019-05-06 13:53:47 -0600

    Add feature tests for search results having logo if UofA holding
    - Added tests for uofa holdings having logo and non-uofa holdings not having logo

---
+++ spec/features/search_uofa_holdings_logo_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Search results UofA holdings logo', type: :feature do
+  scenario 'Search results have logo if UofA holding' do
+    visit '/'
+
+    fill_in 'q', with: ' '
+    click_button 'search'
+
+    click_on 'Library'
+    within('.blacklight-location_tesim') do
+      click_on SYMPHONY_LIBRARY_LOCATIONS[:uainternet]
+    end
+
+    page.all('.document.blacklight-journal').each do |element|
+      expect(element).to have_css("img[src*='ualib-logo.png']")
+    end
+  end
+
+  scenario 'Search results do not have logo if not UofA holding' do
+    visit '/'
+
+    fill_in 'q', with: ' '
+    click_button 'search'
+
+    click_on 'Library'
+    within('.blacklight-location_tesim') do
+      click_on SYMPHONY_LIBRARY_LOCATIONS[:grmacewan]
+    end
+
+    page.all('.document.blacklight-journal').each do |element|
+      expect(element).not_to have_css("img[src*='ualib-logo.png']")
+    end
+  end
+end
