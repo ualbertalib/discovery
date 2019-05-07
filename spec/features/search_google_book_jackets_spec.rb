a154d681ae8f8c5252e855fd0e288526b9253c5f
ConnorSheremeta <sheremet@ualberta.ca>
2019-05-07 10:28:10 -0600

    Add feature tests for display of google book jackets
    - Added tests for google book jackets displaying/not displaying

---
+++ spec/features/search_google_book_jackets_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Search results google book jackets', type: :feature do
+  scenario 'Search results with an isbn have a google book jacket' do
+    visit '/'
+
+    fill_in 'q', with: 'Savages; a film'
+    click_button 'search'
+
+    expect(page).to have_css("img[src*='https://books.google.com/books?vid=ISBN']")
+  end
+
+  scenario 'Search results without an isbn do not have a google book jacket' do
+    visit '/'
+
+    fill_in 'q', with: ' The college Shakespeare; 15 plays and the sonnets'
+    click_button 'search'
+
+    expect(page).not_to have_css("img[src*='https://books.google.com/books?vid=ISBN']")
+  end
+end
