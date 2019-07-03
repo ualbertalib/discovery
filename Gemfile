c739f5525c1a20b8998d189c87bba2bced8d6e8a
Connor Sheremeta <sheremet@ualberta.ca>
2019-07-03 12:21:25 -0600

    Change Layout of Record page
    - Changed record page layout to reflect mock-up
    - Added font-awesome gem
    - Removed remaining glyphicons assets (removed from bootstrap in version 4)
    - Changed call number font to make decimal more visible

--- Gemfile
+++ Gemfile
@@ -8,16 +8,18 @@ gem 'rails', '4.2.11.1'
 gem 'mysql2', '~> 0.4.10'
 
 # Assets (CSS/JS) stuff
+gem 'bootstrap', '~> 4.3.1'
+gem 'font-awesome-sass', '~> 5.8.1'
 gem 'jquery-rails'
 gem 'sassc-rails', '~> 2.1'
 gem 'uglifier', '>= 1.3.0'
 
 # blacklight stuff
-gem 'blacklight', '5.15.0'
-gem 'blacklight-marc', '~> 5.10.0'
+gem 'blacklight', git: 'https://github.com/ualbertalib/blacklight'
+gem 'blacklight-marc', git: 'https://github.com/ualbertalib/blacklight-marc'
 gem 'blacklight_advanced_search'
 gem 'blacklight_google_analytics'
-gem 'blacklight_range_limit', '~> 5.2.0'
+gem 'blacklight_range_limit', git: 'https://github.com/ualbertalib/blacklight_range_limit'
 
 # Authentication
 gem 'devise'
--- app/assets/javascripts/ualib-discovery.js
+++ app/assets/javascripts/ualib-discovery.js
@@ -16,3 +16,26 @@ function setiFrameAttr() {
 }
 
 window.onload=setiFrameAttr;
+
+// Following function taken from blacklight/app/javascript/blacklight/facet_load.js of Blacklight 7 to get proper facet-count widths.
+(function($) {
+  'use strict';
+
+  Blacklight.doResizeFacetLabelsAndCounts = function() {
+    // adjust width of facet columns to fit their contents
+    function longer (a,b){ return b.textContent.length - a.textContent.length; }
+
+    $('ul.facet-values, ul.pivot-facet').each(function(){
+      var longest = $(this).find('span.facet-count').sort(longer)[0];
+
+      if (longest && longest.textContent) {
+        var width = longest.textContent.length + 1 + 'ch';
+        $(this).find('.facet-count').first().width(width);
+      }
+    });
+  };
+
+  Blacklight.onLoad(function() {
+    Blacklight.doResizeFacetLabelsAndCounts();
+  });
+})(jQuery);
