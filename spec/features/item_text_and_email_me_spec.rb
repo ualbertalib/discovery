42b0121f7359205610e139e7fde07799564b807e
Connor Sheremeta <sheremet@ualberta.ca>
2019-08-01 16:20:16 -0600

    Feature tests for text/email me features (#1724)
    - Ensured that text and email me features are functioning

---
+++ spec/features/item_text_and_email_me_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Item can be sent to a user', type: :feature do
+  scenario 'Item can be emailed to a user' do
+    VCR.use_cassette('item_email_text_me') do
+      visit '/catalog/1002481'
+
+      within('.show-tools') do
+        click_link 'Email this'
+      end
+
+      fill_in 'to', with: 'test@test.ca'
+      fill_in 'message', with: 'test message'
+      expect do
+        click_button 'Send'
+        expect(page).to have_content 'Email Sent'
+      end.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+  end
+
+  scenario 'Item can be texted to a user' do
+    VCR.use_cassette('item_email_text_me') do
+      visit '/catalog/1002481'
+
+      within('.show-tools') do
+        click_link 'Text this'
+      end
+
+      fill_in 'to', with: '1234567890'
+      select 'Bell', from: 'carrier'
+      expect do
+        click_button 'Send'
+        expect(page).to have_content 'SMS Sent'
+      end.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+  end
+end
