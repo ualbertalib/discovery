538944b611a6579abe72aa6bc1a137c9cf3f033f
Tricia Jenkins <tricia.g.jenkins@gmail.com>
2019-08-06 15:28:29 -0600

    remove SYMPHONY_ITEM_TYPES

--- config/initializers/symphony_lookup_tables.rb
+++ config/initializers/symphony_lookup_tables.rb
@@ -1,6 +1,3 @@
 # Set up the app-wide +LIBRARY_LOCATIONS+ hash from <tt>config/locations.yml</tt>
 library_locations = YAML.load_file("#{Rails.root}/db/seeds/location.yml")
 SYMPHONY_LIBRARY_LOCATIONS = library_locations.deep_symbolize_keys.freeze
-
-item_types = YAML.load_file("#{Rails.root}/db/seeds/item_type.yml")
-SYMPHONY_ITEM_TYPES = item_types.deep_symbolize_keys.freeze
