7418279d01b2fa32392de21ad17722f5f31e4365
ConnorSheremeta <sheremet@ualberta.ca>
2020-10-16 11:18:12 -0600

    remove skip

--- spec/features/item_ill_link_spec.rb
+++ spec/features/item_ill_link_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Link to ill', type: :feature do
-  unless ENV['TRAVIS']
     scenario 'Sfx item has link to ill' do
       VCR.use_cassette('item_ill_link') do
         visit '/catalog/954921333007'
@@ -9,7 +8,7 @@ RSpec.describe 'Link to ill', type: :feature do
         expect(page).to have_link('ill', href: %r{https://license-terms.library.ualberta.ca\/terms\/})
       end
     end
-  end
+
   scenario 'Non-sfx item does not have link to ill' do
     VCR.use_cassette('item_no_ill_link') do
       visit '/catalog/1002481'
